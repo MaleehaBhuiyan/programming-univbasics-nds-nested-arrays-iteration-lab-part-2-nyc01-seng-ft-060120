ce1439ea23832b1b2ca70ef272bea9827b6f0732
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-19 13:45:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,19 +2,19 @@
 # Produce a new Array that contains the smallest number of each of the nested arrays
 def find_min_in_nested_arrays(src)
   min_array = []
-  row_index = 0 
-  while row_index < src.count do 
-    element_index = 0 
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
     min_value = 100
-    while element_index < src[row_index].count do 
+    while element_index < src[row_index].count do
       if spice_rack[row_index][element_index] < min_value
         min_value = sprc[row_index][element_index]
-      end 
-      element_index += 1 
-    end 
+      end
+      element_index += 1
+    end
     min_array << longest_string_element
-    row_index += 1 
-  end     
+    row_index += 1
+  end
 end
 
 def def find_min_in_nested_arrays(src)
